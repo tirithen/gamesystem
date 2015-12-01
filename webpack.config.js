20617c9fc1250141807353a348a701fc63f30d71
Fredrik Söderström <tirithen@gmail.com>
2015-12-01 21:10:45 +0100

    Lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,37 @@
-(() => {
-  'use strict';
+'use strict';
 
-  let WebpackNotifierPlugin = require('webpack-notifier');
-  let HtmlWebpackPlugin = require('html-webpack-plugin');
+let WebpackNotifierPlugin = require('webpack-notifier');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
 
-  module.exports = {
-    entry: {
-      canvas: './example/canvas/main.js'
-    },
-    output: {
-      path: __dirname + '/build',
-      filename: '[name]/script.js'
-    },
-    devtool: 'cheap-source-map',
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          query: {
-            presets: ['es2015', 'stage-0']
-          }
+module.exports = {
+  entry: {
+    canvas: './example/canvas/main.js'
+  },
+  output: {
+    path: __dirname + '/build',
+    filename: '[name]/script.js'
+  },
+  devtool: 'cheap-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0']
         }
-      ]
-    },
-    plugins: [
-      new WebpackNotifierPlugin(),
-      new HtmlWebpackPlugin({
-        title: 'Basic canvas example',
-        filename: './canvas/index.html',
-        template: './example/template.html',
-        inject: 'body',
-        chunks: ['canvas']
-      })
+      }
     ]
-  };
-}());
+  },
+  plugins: [
+    new WebpackNotifierPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Basic canvas example',
+      filename: './canvas/index.html',
+      template: './example/template.html',
+      inject: 'body',
+      chunks: ['canvas']
+    })
+  ]
+};
